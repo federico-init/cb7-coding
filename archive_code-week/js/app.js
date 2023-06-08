f8a10d959c2e87d38d1bef3670c7b83e89e28e0f
unknown <fefe.borrometi@gmail.com>
2023-06-08 03:39:21 +0200

    third commit

--- archive_code-week/js/app.js
+++ archive_code-week/js/app.js
@@ -1,17 +1,44 @@
-import { genUser, qS } from "./utils/fn.js";
+import { createElement, isUserActive, qS, renderUserList } from "./utils/fn.js";
 import { GET } from "./utils/http.js";
+
+export const userContainer = createElement("div", "", {
+  name: "class",
+  value: "user__container",
+});
+
+const rootEl = qS(".root");
+rootEl.append(userContainer);
+
+const searchBox = qS(".navbar__search__input");
 
 // creo un array dove pusho gli elementi che prelevo tramite GET
 export let userList = [];
 
-const rootEl = qS(".root");
-
 // eseguo la chiamata GET e mi salvo gli utenti
 GET("/users").then((data) => {
   data.forEach((user) => {
+    if (isUserActive()) user.active = true;
+    else user.active = false;
     userList.push(user);
-    rootEl.append(genUser(user));
+    renderUserList(user);
   });
 });
 
 console.log(userList);
+
+// aggiungo un event listener al box di ricerca utente
+searchBox.addEventListener("input", (event) => {
+  userContainer.textContent = "";
+
+  if (event.target.value != "") {
+    userList
+      .filter((user) =>
+        user.name.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+      .forEach((item) => renderUserList(item));
+  }
+  // se pulisco la search box mostro di nuovo tutta la lista
+  else {
+    userList.forEach((user) => renderUserList(user));
+  }
+});
--- archive_code-week/js/utils/fn.js
+++ archive_code-week/js/utils/fn.js
@@ -1,3 +1,5 @@
+import { userContainer } from "../app.js";
+
 // definisco una funzione per la selezione di uno o più elementi
 export const qS = (element) => document.querySelector(element);
 
@@ -12,7 +14,14 @@ export const createElement = (type, content, ...attrs) => {
   return element;
 };
 
-//TODO: creare button filtri
+export const createFilterButtons = () => {
+  const wrapperEl = createElement("div", "", {
+    name: "class",
+    value: "user__filterContainer",
+  });
+
+  const allBtnEl = createElement("div", "All Users");
+};
 
 export const genUser = (userData) => {
   const wrapperEl = createElement("div", "", {
@@ -25,12 +34,17 @@ export const genUser = (userData) => {
     value: "user__profile",
   });
 
+  const userImgContainerEl = createElement("div", "", {
+    name: "class",
+    value: "user__imageContainer",
+  });
+
   const userImageEl = createElement(
     "img",
     "",
     {
       name: "src",
-      value: `./img/${userData.id}.png`,
+      value: `./img/users/${userData.id}.png`,
     },
     {
       name: "alt",
@@ -57,9 +71,24 @@ export const genUser = (userData) => {
     value: "user__username",
   });
 
-  const userEmailEl = createElement("a", userData.email, {
+  const userContactEl = createElement("div", "", {
     name: "class",
-    value: "user__email",
+    value: "user__contacts",
+  });
+
+  const userEmailEl = createElement(
+    "a",
+    userData.email,
+    {
+      name: "class",
+      value: "user__email",
+    },
+    { name: "href", value: `mailto:${userData.email}` }
+  );
+
+  const userPhoneEl = createElement("a", userData.phone, {
+    name: "class",
+    value: "user__phone",
   });
 
   const userWebsiteEl = createElement("a", userData.website, {
@@ -67,11 +96,31 @@ export const genUser = (userData) => {
     value: "user__website",
   });
 
+  const detailsBtnEl = createElement("button", "", {
+    name: "class",
+    value: "fa-regular fa-eye",
+  });
+
+  userImgContainerEl.append(userImageEl);
+
   userNameUsernameEl.append(userNameEl, userAliasEl);
 
-  userProfileEl.append(userImageEl, userNameUsernameEl);
+  userProfileEl.append(userImgContainerEl, userNameUsernameEl);
 
-  wrapperEl.append(userProfileEl, userEmailEl, userWebsiteEl);
+  userContactEl.append(userEmailEl, userPhoneEl);
+
+  wrapperEl.append(userProfileEl, userContactEl, userWebsiteEl, detailsBtnEl);
+
+  if (userData.active) wrapperEl.classList.add("activeUser");
+  else wrapperEl.classList.add("inactiveUser");
 
   return wrapperEl;
 };
+
+// definisco una funzione per l'assegnazione del valore attivo/inattivo per ogni utente
+// in questo caso genero un valore pseudo random che userò per determinare lo stato
+export const isUserActive = () => {
+  if (Math.floor(Math.random() * 2) === 1) return true;
+};
+
+export const renderUserList = (user) => userContainer.append(genUser(user));
